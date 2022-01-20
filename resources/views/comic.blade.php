37c717e30b8b6d08d3ad35f5c38b63b695a3bc71
abdi-El <abdila1099@gmail.com>
2022-01-20 16:24:25 +0100

    finished detail page

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -1,4 +1,36 @@
 @extends('layouts/main')
 @section('content')
-    gg
+        <section class="comic-details">
+            <div class="hero">
+                <div class="container">
+                    <img src="{{$comic['thumb']}}" alt="{{$comic['title']}}">
+                </div>
+            </div>
+            <div class="blue-bar blue-bar-big"></div>
+            <div class="info">
+                <div class="container">
+                    <div class="left">
+                        <h2>{{strtoupper($comic['title'])}}</h2>
+                        <div class="price-availability">
+                            <div class="price">
+                                <span><span class="color-g">U.S. Price: </span>{{$comic['price']}}</span>
+                                <span>
+                                    AVAILABLE
+                                </span>
+                            </div>
+                            <div class="availability">
+                                <span>Check Availability &#10225;</span>
+                            </div>
+                        </div>
+                        <p class="description">
+                            {{$comic['description']}}
+                        </p>
+                    </div>
+                    <div class="right">
+                        {{-- <img src="{{asset('images/dc-logo.png')}}" alt="ads"> --}}
+                    </div>
+                </div>
+            </div>
+            <div class="blue-bar"></div>
+        </section>
 @endsection
